daf8892b135117da9145741583e578d746aa2c0e
Alex Carroll <alex.carroll@us.ibm.com>
2017-10-04 16:32:41 -0700

    find the type of a triangle

---
+++ app.js
@@ -0,0 +1,35 @@
+const comm = require("commander");
+const chalk = require("chalk");
+const Triangle = require("./triangle.js");
+
+
+//deal with arguments being passed.
+comm.option('-t, --triangle <sides>', 'Sides of a triangle: "3, 4, 5"', function(sides){
+    sides = sides.replace(/\s/g, '').split(',')
+    return sides;
+}).parse(process.argv);
+
+//create the triangle
+const tri = new Triangle(comm.triangle);
+
+//catch those errors
+if(tri.type !== null && typeof tri.type === 'object'){
+    if(tri.type.name == "Error"){
+        return console.log(`${chalk.bold.red("Error: ")} ${tri.input} is not a valid input. Use three integers: "3, 4, 5"
+        `);
+    }
+}
+
+//english is fun
+let article = `an`;
+if(tri.type === 'scalene'){
+    article = 'a';
+}
+
+//and finally, if there is no error, give a response
+if ( !tri.type ){
+    return console.log(`${chalk.bold.green("Answer: ")} ${tri.input} ${chalk.red("does not")} make a triangle.
+    `)
+}
+return console.log(`${chalk.bold.green("Answer: ")} ${tri.input} makes ${article} ${chalk.green(tri.type)} triangle.
+`);
---
+++ triangle.js
@@ -0,0 +1,36 @@
+class Triangle {
+    
+        constructor(values){
+            this.input = values;
+            this.triangle = this.input.map(Number).sort((a, b) => a - b);
+        }
+    
+        get type(){
+            try {
+                if ( this.triangle.every(val => parseInt(val)) && this.triangle.length === 3 ){
+                    return this.determineType(this.triangle);
+                }
+                throw new Error();
+            }
+            catch ( error ){
+                return error;
+            }
+            
+        }
+    
+        determineType(){
+            if ( this.triangle[0] === this.triangle[1] && this.triangle[1] === this.triangle[2] ){
+                return "equilateral"
+            }
+            if ( (this.triangle[0] === this.triangle[1] || this.triangle[0] == this.triangle[2] || this.triangle[1] === this.triangle[2]) 
+                && ( (this.triangle[0] + this.triangle[1]) > this.triangle[2] ) ){
+                return "isosceles"
+            }
+            if ( (this.triangle[0] + this.triangle[1]) > this.triangle[2] ){
+                return "scalene"
+            }
+            return false;
+        }
+}
+
+module.exports = Triangle;
