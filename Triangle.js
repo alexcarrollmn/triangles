aed38a1b5bf93d3fe9f8897b3493ab0d82971b98
Alex Carroll <alex.carroll@us.ibm.com>
2017-10-04 18:10:13 -0700

    adding some documentation for Triangle.js

--- Triangle.js
+++ Triangle.js
@@ -1,10 +1,22 @@
 class Triangle {
-    
+    /**
+     * Creates an instance of Triangle.
+     * @param {array} values three sides of a triangle, integers
+     * 
+     * @memberOf Triangle
+     */
     constructor(values){
         this.input = values;
         this.triangle = this.input.map(Number).sort((a, b) => a - b);
     }
 
+    /**
+     * Get the type of triangle, handle errors in input
+     * 
+     * @readonly
+     * @returns {string} type of triangle or error
+     * @memberOf Triangle
+     */
     get type(){
         try {
             if ( this.triangle.every(val => parseInt(val)) && this.triangle.length === 3 ){
@@ -18,6 +30,13 @@ class Triangle {
         
     }
 
+    /**
+     * determins the type of triangle.
+     * 
+     * @returns {string} type
+     * 
+     * @memberOf Triangle
+     */
     determineType(){
         if ( this.triangle[0] === this.triangle[1] && this.triangle[1] === this.triangle[2] ){
             return "equilateral";
