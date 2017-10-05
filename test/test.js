3b3932c3b616c0cb0c0989f1615e4966f43adcbe
Alex Carroll <alex.carroll@us.ibm.com>
2017-10-04 19:15:55 -0700

    fixing descriptions in test

--- test/test.js
+++ test/test.js
@@ -12,21 +12,21 @@ describe('Triangle', function(){
     describe('isosceles', function(){
         var tri = new Triangle([3, 3, 4]);
         console.log(tri.type);
-        it('should return scalene with [3, 3, 4]', function(){
+        it('should return isosceles with [3, 3, 4]', function(){
             expect(tri.type).to.equal('isosceles');
         });
     });
     describe('equilateral', function(){
         var tri = new Triangle([3, 3, 3]);
         console.log(tri.type);
-        it('should return scalene with [3, 3, 3]', function(){
+        it('should return equilateral with [3, 3, 3]', function(){
             expect(tri.type).to.equal('equilateral');
         });
     });
     describe('not a triangle', function(){
         var tri = new Triangle([1, 1, 4]);
         console.log(tri.type);
-        it('should return scalene with [1, 1, 4]', function(){
+        it('should return false with [1, 1, 4]', function(){
             expect(tri.type).to.equal(false);
         });
     });
